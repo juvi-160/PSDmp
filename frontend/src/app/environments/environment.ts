29fe4649eea9387c68eca3bfb4c00f49424f4e80
makkipasha <makkipasha@gmail.com>
2025-08-11 12:35:35 +0300

    firebase config changed

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -7,14 +7,23 @@ export const environment = {
     clientId: 'DTt2AVJqmqoNeYOEArqFvn2CmPtt9eKI',
     audience: 'https://dev-dmocg784jvwai2hb.us.auth0.com/api/v2/',
   },
+  // firebase: {
+  //   apiKey: 'AIzaSyC6__rQOqNeuW2nMJwiBAgXjw‑7Fr6lDb4',
+  //   authDomain: 'psf-membership.firebaseapp.com',
+  //   projectId: 'psf-membership',
+  //   storageBucket: 'psf‑membership.appspot.com',
+  //   messagingSenderId: '447894410735',
+  //   appId: '1:447894410735:web:b915f6ed0963cb6af1521a',
+  //   measurementId: 'G‑6C1FX4YNL3'
+  // },
   firebase: {
-    apiKey: 'AIzaSyC6__rQOqNeuW2nMJwiBAgXjw‑7Fr6lDb4',
-    authDomain: 'psf-membership.firebaseapp.com',
-    projectId: 'psf-membership',
-    storageBucket: 'psf‑membership.appspot.com',
-    messagingSenderId: '447894410735',
-    appId: '1:447894410735:web:b915f6ed0963cb6af1521a',
-    measurementId: 'G‑6C1FX4YNL3'
+    apiKey: "AIzaSyC6__rQOqNeuW2nMJwiBAgXjw-7Fr6lDb4",
+    authDomain: "psf-membership.firebaseapp.com",
+    projectId: "psf-membership",
+    storageBucket: "psf-membership.firebasestorage.app",
+    messagingSenderId: "447894410735",
+    appId: "1:447894410735:web:b915f6ed0963cb6af1521a",
+    measurementId: "G-6C1FX4YNL3"
   },
   razorpayKeyId: 'rzp_live_JD0PZAEXejiVMZ',
   membershipFee: 100000
